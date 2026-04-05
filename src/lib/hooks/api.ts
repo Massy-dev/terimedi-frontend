8ae11a2201a140977518573334f33e1b9e220279
Massy-dev <massy1833.dev@gmail.com>
2026-04-05 12:58:53 +0000

    Modification de redirection v3

--- src/lib/hooks/api.ts
+++ src/lib/hooks/api.ts
@@ -63,7 +63,7 @@ const getAuthToken = (): string | null => {
   if (typeof window === "undefined") return null;
   return localStorage.getItem("auth_token");
 };
-alert("token dans api.ts : "+getAuthToken());
+console.log("token dans api.ts : "+getAuthToken()+" et token dans localStorage : "+localStorage.getItem("auth_token"));
 api.interceptors.request.use((config) => {
   const token = getAuthToken();
   if (token && config.headers) {
